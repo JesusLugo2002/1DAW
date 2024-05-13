5d1aa51c0b2bc26e4af590d85ebae4e8711ce8e6
Jesús Lugo <jesusdlugo02@gmail.com>
2024-05-13 09:06:46 +0100

    Se hacen ejercicios de ETS y LND

---
+++ ETS/unidad-07/tareas/tarea04/patron-factory.py
@@ -0,0 +1,21 @@
+class EnemyGenerator:
+    def spawner_timer(self):
+        # Código del spawner_timer
+        pass
+    def create_enemy()
+    
+
+class GoombaGenerator(EnemyGenerator):
+    pass
+
+class TurtleGenerator(EnemyGenerator):
+    pass
+
+class Enemy:
+    pass
+
+class Goomba(Enemy):
+    pass
+
+class Turtle(Enemy):
+    pass
